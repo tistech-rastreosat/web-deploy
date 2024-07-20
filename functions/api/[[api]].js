8dc878a68b12126593fcd3bcef8daae80837c563
jcardus <joaquim.cardeira@gmail.com>
2024-07-20 20:03:16 -0300

    try http

--- functions/api/[[api]].js
+++ functions/api/[[api]].js
@@ -1,5 +1,5 @@
 export function onRequest({request, env}) {
-    const url = new URL(request.url)
+    const url = new URL(request.url.replace('https://', 'http://'))
     url.host = env.TRACCAR_SERVER
     return fetch(new Request(url, request))
 }
