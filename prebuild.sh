8a8011b569a434d3e40201ab9aed73c89566d375
jcardus <asklocation.net@gmail.com>
2024-08-24 03:34:44 +0100

    use cat

--- prebuild.sh
+++ prebuild.sh
@@ -3,4 +3,4 @@ cp -vr src public index.html traccar-web
 sed -i "s/\${title}/$TITLE/g" traccar-web/vite.config.js
 sed -i "s/\${description}/$DESCRIPTION/g" traccar-web/vite.config.js
 sed -i "s/\${colorPrimary}/$COLOR_PRIMARY/g" traccar-web/vite.config.js
-echo traccar-web/vite.config.js
+cat traccar-web/vite.config.js
